815438f20ec03fc968cc84a17e1720871a6b366a
Valentin <guillaumevalentin76@gmail.com>
2025-04-24 19:26:24 +0200

    secure to false for mailersend

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -12,7 +12,7 @@ const zohoTransporter = nodemailer.createTransport({
 
 const mailersendTransporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST_MAILERSEND,
-  secure: true,
+  secure: false, // Setting secure to false does not mean that you would not use an encrypted connection. Most SMTP servers allow connection upgrade via the STARTTLS command, but to use this, you have to connect using plaintext first. => from https://nodemailer.com/smtp/
   port: parseInt(process.env.EMAIL_PORT_MAILERSEND),
   auth: {
     user: process.env.EMAIL_ADDRESS_MAILERSEND,
