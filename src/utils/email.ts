7d30c21953342ce248ad3355ba52c209d7d494e0
Valentin <guillaumevalentin76@gmail.com>
2025-04-24 19:33:54 +0200

    Adding from for mailersend as need

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -14,6 +14,7 @@ const mailersendTransporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST_MAILERSEND,
   secure: false, // Setting secure to false does not mean that you would not use an encrypted connection. Most SMTP servers allow connection upgrade via the STARTTLS command, but to use this, you have to connect using plaintext first. => from https://nodemailer.com/smtp/
   port: parseInt(process.env.EMAIL_PORT_MAILERSEND),
+  from: process.env.EMAIL_DOMAIN_MAILERSEND,
   auth: {
     user: process.env.EMAIL_ADDRESS_MAILERSEND,
     pass: process.env.EMAIL_PASSWORD_MAILERSEND,
