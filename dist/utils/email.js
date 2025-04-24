f18fbbcf0c4714a5ae63a9f03958f50ae51afb6d
Valentin <guillaumevalentin76@gmail.com>
2025-04-24 19:14:47 +0200

    Adding a new transporter

--- dist/utils/email.js
+++ dist/utils/email.js
@@ -1,20 +1,29 @@
 import nodemailer from "nodemailer";
-const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
+const zohoTransporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST_ZOHO,
     secure: true,
-    port: 465,
+    port: parseInt(process.env.EMAIL_PORT_ZOHO),
     auth: {
-        user: process.env.EMAIL_ADDRESS,
-        pass: process.env.EMAIL_PASSWORD,
+        user: process.env.EMAIL_ADDRESS_ZOHO,
+        pass: process.env.EMAIL_PASSWORD_ZOHO,
     },
 });
-const sendEmail = async (recipient, subject, text) => {
+const mailersendTransporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST_SENDMAILER,
+    secure: true,
+    port: parseInt(process.env.EMAIL_PORT_SENDMAILER),
+    auth: {
+        user: process.env.EMAIL_ADDRESS_SENDMAILER,
+        pass: process.env.EMAIL_PASSWORD_SENDMAILER,
+    },
+});
+const sendEmail = async (recipient, subject, text, transporterName = "SENDMAILER") => {
     const mailOptions = {
         from: process.env.EMAIL_ADDRESS,
         to: recipient,
         subject: subject,
         text: text,
     };
-    return transporter.sendMail(mailOptions);
+    return (transporterName === "SENDMAILER" ? mailersendTransporter : zohoTransporter).sendMail(mailOptions);
 };
 export { sendEmail };
--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,16 +1,31 @@
 import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
+const zohoTransporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST_ZOHO,
   secure: true,
-  port: 465,
+  port: parseInt(process.env.EMAIL_PORT_ZOHO),
   auth: {
-    user: process.env.EMAIL_ADDRESS,
-    pass: process.env.EMAIL_PASSWORD,
+    user: process.env.EMAIL_ADDRESS_ZOHO,
+    pass: process.env.EMAIL_PASSWORD_ZOHO,
   },
 });
 
-const sendEmail = async (recipient: string, subject: string, text: string) => {
+const mailersendTransporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST_SENDMAILER,
+  secure: true,
+  port: parseInt(process.env.EMAIL_PORT_SENDMAILER),
+  auth: {
+    user: process.env.EMAIL_ADDRESS_SENDMAILER,
+    pass: process.env.EMAIL_PASSWORD_SENDMAILER,
+  },
+});
+
+const sendEmail = async (
+  recipient: string,
+  subject: string,
+  text: string,
+  transporterName: "ZOHO" | "SENDMAILER" = "SENDMAILER"
+) => {
   const mailOptions = {
     from: process.env.EMAIL_ADDRESS,
     to: recipient,
@@ -18,7 +33,9 @@ const sendEmail = async (recipient: string, subject: string, text: string) => {
     text: text,
   };
 
-  return transporter.sendMail(mailOptions);
+  return (
+    transporterName === "SENDMAILER" ? mailersendTransporter : zohoTransporter
+  ).sendMail(mailOptions);
 };
 
 export { sendEmail };
