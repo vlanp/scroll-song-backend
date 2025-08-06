1a811c2f2084df02175f90dacf80c4fc18cfccba
Valentin GUILLAUME <guillaume.valentin76@gmail.com>
2025-08-06 14:57:54 +0200

    email.ts updated to use mailgun

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -21,15 +21,28 @@ const mailersendTransporter = nodemailer.createTransport({
   },
 });
 
+const mailgunTransporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST_MAILGUN,
+  secure: false, // Setting secure to false does not mean that you would not use an encrypted connection. Most SMTP servers allow connection upgrade via the STARTTLS command, but to use this, you have to connect using plaintext first. => from https://nodemailer.com/smtp/
+  port: parseInt(process.env.EMAIL_PORT_MAILGUN),
+  from: process.env.EMAIL_ADDRESS_MAILGUN,
+  auth: {
+    user: process.env.EMAIL_USERNAME_MAILGUN,
+    pass: process.env.EMAIL_PASSWORD_MAILGUN,
+  },
+});
+
 const sendEmail = async (
   recipient: string,
   subject: string,
   text: string,
-  transporterName: "ZOHO" | "MAILERSEND" = "MAILERSEND"
+  transporterName: "ZOHO" | "MAILERSEND" | "MAILGUN" = "MAILGUN"
 ) => {
   const mailOptions = {
     from:
-      transporterName === "MAILERSEND"
+      transporterName === "MAILGUN"
+        ? process.env.EMAIL_ADDRESS_MAILGUN
+        : transporterName === "MAILERSEND"
         ? process.env.EMAIL_ADDRESS_MAILERSEND
         : process.env.EMAIL_ADDRESS_ZOHO,
     to: recipient,
@@ -38,7 +51,11 @@ const sendEmail = async (
   };
 
   return (
-    transporterName === "MAILERSEND" ? mailersendTransporter : zohoTransporter
+    transporterName === "MAILGUN"
+      ? mailgunTransporter
+      : transporterName === "MAILERSEND"
+      ? mailersendTransporter
+      : zohoTransporter
   ).sendMail(mailOptions);
 };
 
